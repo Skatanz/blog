fcb0daff0810cef0f1159b067aee8a871a058b0c
Y.Yamada <yasutai33@gmail.com>
2018-10-04 23:03:57 +0900

    vagrantで構築

--- index.php
+++ index.php
@@ -3,11 +3,53 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>ブログ</title>
+    <title>ブログtest</title>
 </head>
 
+<header>
+    <div>
+        <h1>ブログタイトル</h1>
+    </div>
+</header>
+
 <body>
-    ここにページの中身を記述
+    <div>
+        <div>
+            <a href="">
+                <p> <?php "1記事目タイトル" ?> </p>
+            </a>
+        </div>
+
+        <div>
+            <a href="">
+                <p> <?php "2記事目タイトル" ?> </p>
+            </a>
+        </div>
+
+        <div>
+            <a href="">
+                <p> <?php "3記事目タイトル" ?> </p>
+            </a>
+        </div>
+
+        <div>
+            <a href="">
+                <p> <?php "4記事目タイトル" ?> </p>
+            </a>
+        </div>
+
+        <div>
+            <a href="">
+                <p> <?php "5記事目タイトル" ?> </p>
+            </a>
+        </div>    
+    </div>
 </body>
+
+<footer>
+    <div>
+        <a href="">管理者ログイン</a>
+    </div>
+</footer>
 
 </html>
---
+++ kiji.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title> <?php "タイトル" ?> </title>
+</head>
+
+<header>
+    <div>
+        <h1>ブログタイトル</h1>
+    </div>
+</header>
+
+<body>
+    <div>
+        <h2> <?php "タイトル" ?> </h2>
+        <div>
+            <pre> <?php "記事" ?> </pre>
+        </div>
+    </div>
+</body>
+
+
+</html>
